91e262e657de049cf3f7093e56a5f43c21407b49
9thLevelWizard <154519142+9thLevelWizard@users.noreply.github.com>
2024-01-31 14:05:00 +0000

    Feat: Computer makes its choice
    Added a function that randomly determines the computer player's choice.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,21 @@
+function getComputerChoice() {
+    
+    let randomChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1); // Stores a random number between 1 and 3, inclusive, to use as the computer's choice
+    let computerChoice;
+
+    switch (randomChoice) {
+        case 1:
+            computerChoice = "Rock";
+            break;
+        case 2:
+            computerChoice = "Paper";
+            break;
+        case 3:
+            computerChoice = "Scissors";
+            break;
+        default:
+            break;
+    }
+
+    return computerChoice;
+}
